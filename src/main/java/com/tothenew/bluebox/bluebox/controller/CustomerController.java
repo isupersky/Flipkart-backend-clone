88611374442bee10725c4712b013862c93ae5313
iAakashSinha <aakash.sinha@tothenew.com>
2020-03-25 23:01:14 +0530

    email verification token with timestamp check that expires after 3 hours Added.

--- src/main/java/com/tothenew/bluebox/bluebox/controller/CustomerController.java
+++ src/main/java/com/tothenew/bluebox/bluebox/controller/CustomerController.java
@@ -1,14 +1,8 @@
 package com.tothenew.bluebox.bluebox.controller;
 
-import com.tothenew.bluebox.bluebox.enitity.ConfirmationToken;
 import com.tothenew.bluebox.bluebox.enitity.Customer;
-import com.tothenew.bluebox.bluebox.enitity.User;
-import com.tothenew.bluebox.bluebox.repository.ConfirmationTokenRepository;
-import com.tothenew.bluebox.bluebox.repository.CustomerRepository;
 import com.tothenew.bluebox.bluebox.service.CustomerService;
-import com.tothenew.bluebox.bluebox.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,76 +17,28 @@ public class CustomerController {
   @Autowired
   CustomerService customerService;
 
-  @Autowired
-  CustomerRepository customerRepository;
-
-  @Autowired
-  ConfirmationTokenRepository confirmationTokenRepository;
-
-  @Autowired
-  EmailSenderService emailSenderService;
-
-  @PostMapping(path = "/customer-register")
-  public String registerCustomer(@RequestBody Customer customer) {
-    customerService.createCustomer(customer);
-    return "Success".toUpperCase();
-  }
+//  @PostMapping(path = "/customer-register")
+//  public String registerCustomer(@RequestBody Customer customer) {
+//    customerService.createCustomer(customer);
+//    return "Success".toUpperCase();
+//  }
 
   @GetMapping(path = "/customer")
   public Iterable<Customer> getTestUser() {
     return customerService.getTestUser();
   }
 
-  @RequestMapping(value = "/register", method = RequestMethod.POST)
-  public String registerUser(@RequestBody Customer customer) {
+  @PostMapping(value = "/customer-register")
+  public String registerCustomer(@RequestBody Customer customer) {
 
-    User existingUser = customerRepository.findByEmailIgnoreCase(customer.getEmail());
-    if (existingUser != null) {
-      return "message : This email already exists!";
-//      modelAndView.setViewName("error");
-    } else {
-      customerRepository.save(customer);
+    return customerService.registerCustomer(customer);
 
-      ConfirmationToken confirmationToken = new ConfirmationToken(customer);
-
-      confirmationTokenRepository.save(confirmationToken);
-
-      SimpleMailMessage mailMessage = new SimpleMailMessage();
-      mailMessage.setTo(customer.getEmail());
-      mailMessage.setSubject("Complete Registration!");
-      mailMessage.setFrom("ecommerce476@gmail.com ");
-      mailMessage.setText("To confirm your account, please click here : "
-          + "http://localhost:8080/confirm-account?token=" + confirmationToken
-          .getConfirmationToken());
-
-      emailSenderService.sendEmail(mailMessage);
-
-//      modelAndView.addObject("emailId", customer.getEmailId());
-
-      return "successfulRegisteration";
-    }
-
-//    return modelAndView;
   }
 
   @RequestMapping(value = "/confirm-account", method = {RequestMethod.GET, RequestMethod.POST})
   public String confirmUserAccount(@RequestParam("token") String confirmationToken) {
-    ConfirmationToken token = confirmationTokenRepository
-        .findByConfirmationToken(confirmationToken);
 
-    if (token != null) {
-      Customer customer = (Customer) customerRepository
-          .findByEmailIgnoreCase(token.getUser().getEmail());
-      customer.setActive(true);
-      customerRepository.save(customer);
-      confirmationTokenRepository.delete(token);
-      return "accountVerified";
-    } else {
-      return "message: The link is invalid or broken!";
-//      modelAndView.setViewName("error");
-    }
-
-//    return modelAndView;
+    return customerService.validateCustomer(confirmationToken);
   }
 
 }
--- src/main/java/com/tothenew/bluebox/bluebox/service/CustomerService.java
+++ src/main/java/com/tothenew/bluebox/bluebox/service/CustomerService.java
@@ -1,13 +1,19 @@
 package com.tothenew.bluebox.bluebox.service;
 
+import com.tothenew.bluebox.bluebox.enitity.ConfirmationToken;
 import com.tothenew.bluebox.bluebox.enitity.Customer;
 import com.tothenew.bluebox.bluebox.enitity.Role;
+import com.tothenew.bluebox.bluebox.enitity.User;
+import com.tothenew.bluebox.bluebox.repository.ConfirmationTokenRepository;
 import com.tothenew.bluebox.bluebox.repository.CustomerRepository;
 import com.tothenew.bluebox.bluebox.repository.RoleRepository;
 import com.tothenew.bluebox.bluebox.repository.UserRepository;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +28,19 @@ public class CustomerService {
   @Autowired
   CustomerRepository customerRepository;
 
+  @Autowired
+  ConfirmationTokenRepository confirmationTokenRepository;
+
+  @Autowired
+  EmailSenderService emailSenderService;
+
+  @Autowired
+  private TaskExecutor taskExecutor;
+
+  public Iterable<Customer> getTestUser() {
+    return customerRepository.findAll();
+  }
+
   public void createCustomer(Customer customer) {
     List<Role> defaultRole = new ArrayList<>();
     Role role = roleRepository.findById(2).get();
@@ -31,9 +50,72 @@ public class CustomerService {
   }
 
 
-  public Iterable<Customer> getTestUser() {
-    return customerRepository.findAll();
+  public String registerCustomer(Customer customer) {
+    User existingUser = customerRepository.findByEmailIgnoreCase(customer.getEmail());
+    if (existingUser != null) {
+      return "message : This email already exists!";
+    } else {
+      customerRepository.save(customer);
+      generateToken(customer);
+      return "successfulRegisteration";
+    }
+  }
 
-//    return tempList;
+  public void generateToken(Customer customer) {
+    ConfirmationToken confirmationToken = new ConfirmationToken(customer);
+
+    confirmationTokenRepository.save(confirmationToken);
+
+    taskExecutor.execute(new Runnable() {
+      public void run() {
+        try {
+          SimpleMailMessage mailMessage = new SimpleMailMessage();
+          mailMessage.setTo(customer.getEmail());
+          mailMessage.setSubject("Complete Registration!");
+          mailMessage.setFrom("ecommerce476@gmail.com ");
+          mailMessage.setText("To confirm your account, please click here : "
+              + "http://localhost:8080/confirm-account?token=" + confirmationToken
+              .getConfirmationToken());
+
+          emailSenderService.sendEmail(mailMessage);
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.err.println(
+              "Failed to send email to: " + customer.getEmail() + " reason: " + e.getMessage());
+        }
+      }
+    });
+  }
+
+
+  public String validateCustomer(String confirmationToken) {
+
+    ConfirmationToken token = confirmationTokenRepository
+        .findByConfirmationToken(confirmationToken);
+
+    Date date = new Date();
+
+    if (token != null && token.getCreatedDate().getHours() - date.getHours() <= 3) {
+
+      Customer customer = (Customer) customerRepository
+          .findByEmailIgnoreCase(token.getUser().getEmail());
+      customer.setActive(true);
+      customerRepository.save(customer);
+      confirmationTokenRepository.delete(token);
+      return "accountVerified";
+    } else if (token != null && token.getCreatedDate().getHours() - date.getHours() > 3) {
+      Customer customer = customerRepository
+          .findById(
+              token
+                  .getUser()
+                  .getId())
+          .get();
+
+      confirmationTokenRepository.delete(token);
+      generateToken(customer);
+      return "New Token sent to registered email Id";
+    } else {
+      return "message: The link is invalid or broken!";
+    }
   }
 }
