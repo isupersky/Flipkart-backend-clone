31151ad6f02fb722a74e67915888463d91190748
iAakashSinha <aakash.sinha@tothenew.com>
2020-04-06 11:32:25 +0530

    Seller Resiter Api using Seller Dto

--- src/main/java/com/tothenew/bluebox/bluebox/controller/SellerController.java
+++ src/main/java/com/tothenew/bluebox/bluebox/controller/SellerController.java
@@ -1,8 +1,10 @@
 package com.tothenew.bluebox.bluebox.controller;
 
+import com.tothenew.bluebox.bluebox.dto.SellerDto;
 import com.tothenew.bluebox.bluebox.enitity.user.Seller;
 import com.tothenew.bluebox.bluebox.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/sellers")
+@RequestMapping(path = "/seller")
 public class SellerController {
 
   @Autowired
@@ -18,7 +20,7 @@ public class SellerController {
 
 
   @PostMapping(path = "/register")
-  public String register(@RequestBody Seller seller) {
-    return sellerService.registerSeller(seller);
+  public ResponseEntity<Object> register(@RequestBody SellerDto sellerDto) {
+    return sellerService.registerSeller(sellerDto);
   }
 }
--- src/main/java/com/tothenew/bluebox/bluebox/service/SellerService.java
+++ src/main/java/com/tothenew/bluebox/bluebox/service/SellerService.java
@@ -1,13 +1,18 @@
 package com.tothenew.bluebox.bluebox.service;
 
+import com.tothenew.bluebox.bluebox.dto.SellerDto;
 import com.tothenew.bluebox.bluebox.enitity.user.Role;
 import com.tothenew.bluebox.bluebox.enitity.user.Seller;
 import com.tothenew.bluebox.bluebox.enitity.user.User;
+import com.tothenew.bluebox.bluebox.exception.UserAlreadyExistsException;
 import com.tothenew.bluebox.bluebox.repository.RoleRepository;
 import com.tothenew.bluebox.bluebox.repository.SellerRepository;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,18 +29,20 @@ public class SellerService {
   }
 
 
-  public String registerSeller(Seller seller) {
+  public ResponseEntity<Object> registerSeller(SellerDto sellerDto) {
     List<Role> defaultRole = new ArrayList<>();
     Role role = roleRepository.findByAuthority("ROLE_SELLER");
+    defaultRole.add(role);
 
-    User existingUser = sellerRepository.findByEmailIgnoreCase(seller.getEmail());
+    User existingUser = sellerRepository.findByEmailIgnoreCase(sellerDto.getEmail());
     if (existingUser != null) {
-      return "message : This email already exists!";
+      throw new UserAlreadyExistsException("This email already exists!");
     } else {
-      defaultRole.add(role);
+      Seller seller = new Seller();
+      BeanUtils.copyProperties(sellerDto, seller);
       seller.setRoles(defaultRole);
       sellerRepository.save(seller);
-      return "SUCCESS";
+      return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
     }
   }
 
